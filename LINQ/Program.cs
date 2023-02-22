509742d4268636da7c8fa3026bf44d4dcc8a98e8
Jack Kessler <jack_kessler@hotmail.com>
2023-02-22 13:44:10 -0600

    Exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGameList = new List<string>() { "Pokemon", "Halo", "MarioKart", "Zelda", "Madden 2018", "Candy Crush" };
+
+            //Option 1
+            //var sortedList = videoGameList.Where(x => x.Length > 0).OrderByDescending(x => x);
+
+            //foreach ( var videoGame in sortedList )
+            //{
+            //    Console.WriteLine(videoGame);
+            //}
+
+            //Option 2
+            videoGameList.Where(x => x.Length > 0).OrderBy(x => x).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
